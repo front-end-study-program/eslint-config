6b8f6abe81c6f3d44c4b6709cdf29a62b3051b05
yanquanfahei <1449826851@qq.com>
2022-06-24 19:13:07 +0800

    feat: add basic rules

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -1,15 +1,34 @@
 module.exports = {
-  reportUnusedDisableDirectives: true, // 报告未使用 eslint 禁用注释
+  env: {
+    browser: true,
+  },
+  reportUnusedDisableDirectives: true, // 报告未使用 eslint-disable 禁用注释
   extends: [
     "eslint:recommended",
-    "standard"
+    "standard",
   ],
   rules: {
-    'no-unused-vars': 'warn',
+    // Possible Errors
+    'no-debugger': 'off',
+    'no-empty': 'off',
+
+    // Best Practices
+    'default-case': 'warn',
+
+    // Stylistic Issues
+    'jsx-quotes': ['warn', 'prefer-double'],
+    'max-len': 'warn',
+    'object-property-newline': ['warn', { 'allowAllPropertiesOnSameLine': false }],
+
+    // es6
+    'arrow-body-style': ['warn', 'as-needed'],
+    'no-var': 'warn',
     'object-shorthand': ['warn', 'always', {
       avoidQuotes: true,
       avoidExplicitReturnArrows: true
-    }]
+    }],
+    'prefer-arrow-callback': 'warn',
+    'prefer-destructuring': 'warn',
   },
   ignorePatterns: ['dist/*', 'node_modules/*'] // 忽略文件或者目录，使用 glob 模式语法
 }
--- playground/basic/src/index.js
+++ playground/basic/src/index.js
@@ -1,18 +1,66 @@
+import { map, keys } from 'lodash-es'
+
 for (let index = 0; index < 10; index++) {
   const a = 1
 }
 
-Promise((res) => {
-  setTimeout(res, 100)
-})
-
-const a = 1
+Promise((res) => setTimeout(res, 100))
 
 const b = {
   'hello-word': function () {
-    return 'hello'
-  },
-  a
+    return new Promise((resolve) => setTimeout(resolve, 500))
+  }
 }
 
 b.hello()
+
+function test (a, b) {
+  if (a === 1) {
+    return a
+  }
+  const object1Test = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 1
+  }
+  const { h } = object1Test
+  return (<div class="box" b="a">1</div>)
+}
+
+[].map(() => (1))
+
+switch (1) {
+  case 1: {
+    const foo = 1
+    break
+  }
+  default:
+    break
+}
+
+const foo = {
+  a: 1,
+  b: 2,
+  c: 3
+}
+
+alert(1)
+
+const { c } = foo
+
+Buffer.alloc(5)
+
+const bar = process.env
+
+const foo1 = a => ({ bar: a })
+
+module.exports = {
+  bar
+}
+
+export default bar
