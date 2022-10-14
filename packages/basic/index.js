769a91ccbcc4dabff887ebb5339089384dad8d43
yanquanfahei <1449826851@qq.com>
2022-10-14 22:53:35 +0800

    feat: markdown recommended

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -16,14 +16,15 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:import/recommended',
-    "plugin:prettier/recommended"
+    'plugin:prettier/recommended',
+    'plugin:markdown/recommended'
   ],
-  plugins: ['import', 'prettier'],
+  plugins: ['import', 'prettier', 'markdown'],
   overrides: [
     {
-      files: ["*.html", "**/*.html"],
-      plugins: ["html"], // 解析 html
-    }
+      files: ['*.html", "**/*.html'],
+      plugins: ['html'], // 解析 html
+    },
   ],
   rules: {
     // Possible Errors
